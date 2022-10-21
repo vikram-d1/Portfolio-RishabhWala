05a465ee4d62dc85f56837381f608dd8e8968ff9
Vikram <vikramd1001@gmail.com>
2022-10-21 07:58:40 +0530

    Nav Bar fixed

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ hamburger.addEventListener('click', (e) => {
 
     let list = document.querySelector('ul');
 
-    if(e.target.name === 'menu'){
-        e.target.name = 'close';
-        list.classList.toggle('-translate-y-full');
-        list.classList.add('translate-y-50');
-        list.classList.add('transition')
+    list.classList.toggle('-translate-y-full')
+
+    if(hamburger.classList.contains('active')){
+        hamburger.innerHTML ='<i class="fa-solid fa-x "></i>';
+        hamburger.classList.remove('active')
     } else {
-        e.target.name = 'menu';
-        list.classList.toggle('translate-y-50');
-        list.classList.add('-translate-y-full');
+        hamburger.innerHTML = '<i id="menu" class="fa-solid fa-bars active"></i>';
+        hamburger.classList.add('active')
     }
+
+    // if(e.target.name === 'menu'){
+    //     e.target.name = 'close';
+    //     list.classList.toggle('-translate-y-full');
+    //     list.classList.add('translate-y-50');
+    //     list.classList.add('transition')
+    // } else {
+    //     e.target.name = 'menu';
+    //     list.classList.toggle('translate-y-50');
+    //     list.classList.add('-translate-y-full');
+    // }
     
 })
-
 
 
