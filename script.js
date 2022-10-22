67bcb9cf5f04f4eac5d471733b698e88b98d06fb
Vikram <vikramd1001@gmail.com>
2022-10-22 19:06:30 +0530

    changes added

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-const hamburger = document.getElementById('hamburger-menu');
+const hamburger = document.querySelector('nav span button');
+const list = document.querySelector('ul')
 
 hamburger.addEventListener('click', (e) => {
-    console.log(e.target.name)
 
-    let list = document.querySelector('ul');
+    // list.classList.toggle('-translate-y-full');
+    hamburger.classList.toggle('is-active');
 
-    list.classList.toggle('-translate-y-full')
 
-    if(hamburger.classList.contains('active')){
-        hamburger.innerHTML ='<i class="fa-solid fa-x "></i>';
-        hamburger.classList.remove('active')
+    if(hamburger.classList.contains('is-active')){
+        list.classList.add('top-28');
+        list.classList.add('opacity-100');
     } else {
-        hamburger.innerHTML = '<i id="menu" class="fa-solid fa-bars active"></i>';
-        hamburger.classList.add('active')
+        list.classList.remove('top-28');
+        list.classList.remove('opacity-100');
     }
 
-    // if(e.target.name === 'menu'){
-    //     e.target.name = 'close';
-    //     list.classList.toggle('-translate-y-full');
-    //     list.classList.add('translate-y-50');
-    //     list.classList.add('transition')
-    // } else {
-    //     e.target.name = 'menu';
-    //     list.classList.toggle('translate-y-50');
-    //     list.classList.add('-translate-y-full');
-    // }
-    
-})
 
+})
 
